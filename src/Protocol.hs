4bad5fb5bec572bb1d00657230a0efe8f1cecb76
unknown <alvian.better@gmail.com>
2017-11-16 02:03:46 +0800

    Remove irrelevant Protocol, MESI, and Dragon implementation

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -1,16 +1,1 @@
-module Protocol (read, write) where
-
-import Definitions
-import CacheSet
-import Data.Array as Array
-import qualified DragonCacheModifier
-import qualified MESICacheModifier
-import Prelude hiding (read, write)
-
-read :: Protocol -> (BlockTag, SetIndex, Offset) -> Array Int CacheSet -> (IsBusy, IsReadHit, Array Int CacheSet)
-read MESI (blockTag, setIndex, offset) cacheStructure = MESICacheModifier.read (blockTag, setIndex, offset) cacheStructure
-read Dragon (blockTag, setIndex, offset) cacheStructure = DragonCacheModifier.read (blockTag, setIndex, offset) cacheStructure
-
-write :: Protocol -> (BlockTag, SetIndex, Offset) -> Array Int CacheSet -> (IsBusy, IsWriteHit, Array Int CacheSet)
-write MESI (blockTag, setIndex, offset) cacheStructure = MESICacheModifier.write (blockTag, setIndex, offset) cacheStructure
-write Dragon (blockTag, setIndex, offset) cacheStructure = DragonCacheModifier.write (blockTag, setIndex, offset) cacheStructure
+module Protocol where
