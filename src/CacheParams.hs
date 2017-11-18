72b8d5b5d0485777d1890dd3517935e411ec6bc0
Sriram Sami <frizensami@users.noreply.github.com>
2017-11-18 13:42:03 +0800

    Revert "Cache Implementation"

--- src/CacheParams.hs
+++
@@ -1,7 +0,0 @@
-module CacheParams (create) where
-
-import Definitions
-
-create :: CacheSize -> Associativity -> BlockSize -> CacheParams
-create cacheSize associativity blockSize = CacheParams cacheSize associativity blockSize numCacheSets where 
-    numCacheSets = cacheSize `div` (associativity * blockSize)
